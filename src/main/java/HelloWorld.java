bc3700c36b4cdba4f9ecaa30d8586aa8adaf2810
NeemaGobari <141130995+NeemaGobari@users.noreply.github.com>
2023-08-05 13:09:05 +0530

    Update HelloWorld.java

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("This is my first github interaction with jenkinsssss");
+        System.out.println("This is my first github interaction with jenkins");
     }
 }
