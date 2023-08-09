6e7274d9043ce8bf88b12e06eebac785744da3b6
NeemaGobari <141130995+NeemaGobari@users.noreply.github.com>
2023-08-09 14:55:24 +0530

    Update HelloWorld.java

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -2,7 +2,7 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("This is my first github interaction with jenkinssss");
         System.out.println("This is commit from feature into master");
-        System.out.println("This demo is for git rebase from Eclipse");
+        System.outtt.println("This demo is for git rebase from Eclipse");
         
     }
 }
